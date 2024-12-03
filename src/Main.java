7259dbfd630c7519ba7659361c0635f885094543
estewar5 <estewar5@oswego.edu>
2024-12-03 08:26:37 -0500

    Final type check commit

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //Run Good Programs
-        //Testing testGoodFiles = new Testing(false, "test_programs/error_free");
-        //testGoodFiles.runTests();
-
-        //Run Bad Programs
-//        Testing testBadFiles = new Testing(false, "test_programs/error_prone");
-//        testBadFiles.runTests();
-        Testing testTypeChecking = new Testing(false, "test_programs/pls_dont_work");
+        Testing testTypeChecking = new Testing(false, "test_programs/type_error_programs");
         testTypeChecking.runTests();
     }
 }
